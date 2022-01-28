1284417c743f1956dd084caf3e1e6ce023fce12c
RamonRossaDePaula <ramonrossadepaula@hotmail.com>
2022-01-28 15:55:25 -0300

    Commit

--- Atv 1 - Und 1 - Dens. Prob. Espaço Real e K (parte 1).py
+++ Atv 1 - Und 1 - Dens. Prob. Espaço Real e K (parte 1).py
@@ -1,4 +1,15 @@
 
+# Disciplina: Mecânica Quântica (FIS01210)
+# Aluno: Ramon Rossa de Paula (00243967)
+
+# "Este trabalho foi produzido em Python, utilizando a IDE Visual Studio Code, exclusivamente por mim. Nenhuma parte deste trabalho (texto e fórmulas) foi copiada digitalmente de outras fontes"
+
+
+
+
+
+# ____________________________________________________________________________________
+
 # Importações
 
 import math
@@ -39,5 +50,5 @@ media_x_quad,err = integrate.quad(lambda x :x**2*psi0(x)**2,-np.inf,np.inf)
      # Probabilidade, Média e Desvio Padrão:
 
 print ('Área = {:.3f}'.format(probabilidade))                                             # Probabilidade = Integral de -∞ a +∞ de |Ψ(x)|² = 1
-print ('Média = {:.3f}'.format(media_x))                                              # <x> = Integral de -∞ a +∞ de (x * |Ψ(x)|²)
+print ('Média = {:.3f}'.format(media_x))                                                  # <x> = Integral de -∞ a +∞ de (x * |Ψ(x)|²)
 print ('Desvio Padrão = {:.3f}'.format(math.sqrt(media_x_quad-media_x**2)))               # σ² = <x²> - <x>²
